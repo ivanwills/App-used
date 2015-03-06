6636a98fc183a6fbc36bd781820e2a33583fe11b
Ivan Wills <ivan.wills@gmail.com>
2015-03-07 08:48:25 +1100

    Moved file from App-Useful

--- lib/App/used.pm
+++ lib/App/used.pm
@@ -10,31 +10,13 @@ use strict;
 use warnings;
 use version;
 use Carp;
-use Scalar::Util;
-use List::Util;
-#use List::MoreUtils;
 use Data::Dumper qw/Dumper/;
 use English qw/ -no_match_vars /;
 use base qw/Exporter/;
 
-
 our $VERSION     = version->new('0.0.1');
 our @EXPORT_OK   = qw//;
 our %EXPORT_TAGS = ();
-#our @EXPORT      = qw//;
-
-sub new {
-	my $caller = shift;
-	my $class  = ref $caller ? ref $caller : $caller;
-	my %param  = @_;
-	my $self   = \%param;
-
-	bless $self, $class;
-
-	return $self;
-}
-
-
 
 1;
 
@@ -42,7 +24,7 @@ __END__
 
 =head1 NAME
 
-App::used - <One-line description of module's purpose>
+App::used - Find modules used or required in perl files or directories of perl files
 
 =head1 VERSION
 
@@ -51,84 +33,43 @@ This documentation refers to App::used version 0.0.1
 
 =head1 SYNOPSIS
 
-   use App::used;
+   used [option]
 
-   # Brief but working code example(s) here showing the most common usage(s)
-   # This section will be as far as many users bother reading, so make it as
-   # educational and exemplary as possible.
+ OPTIONS:
+  -n --name       Order by module name (Default order)
+  -I --lib[=]dir  Add the directory to the list of local library paths. This
+                  stops reporting of modules that are part of the project
+                  being marked as not added to the Build.PL file. The default
+                  list includes lib/ and t/lib/
+  -u --used       Order by the number of times a module is used/required
+  -U --update     Update the requires section of the Build.PL file
+  -d --decending  Reverse the sort order
 
+  -p --perl-version
+                  Show files that require the highest version of Perl to be used
+  -v --verbose    Show more detailed option
+                    Specified once shows module verion numbers verses required
+                    versions.
+                    Specified twice also shows modules that are local to the
+                    project and modules that are part of the default perl
+                    version.
+     --version    Prints the version information
+     --help       Prints this help information
+     --man        Prints the full documentation for used
 
 =head1 DESCRIPTION
 
-A full description of the module and its features.
-
-May include numerous subsections (i.e., =head2, =head3, etc.).
-
-
 =head1 SUBROUTINES/METHODS
-
-A separate section listing the public components of the module's interface.
-
-These normally consist of either subroutines that may be exported, or methods
-that may be called on objects belonging to the classes that the module
-provides.
-
-Name the section accordingly.
-
-In an object-oriented module, this section should begin with a sentence (of the
-form "An object of this class represents ...") to give the reader a high-level
-context to help them understand the methods that are subsequently described.
-
-
-=head3 C<new ( $search, )>
-
-Param: C<$search> - type (detail) - description
-
-Return: App::used -
-
-Description:
-
-=cut
-
 
 =head1 DIAGNOSTICS
 
-A list of every error and warning message that the module can generate (even
-the ones that will "never happen"), with a full explanation of each problem,
-one or more likely causes, and any suggested remedies.
-
 =head1 CONFIGURATION AND ENVIRONMENT
-
-A full explanation of any configuration system(s) used by the module, including
-the names and locations of any configuration files, and the meaning of any
-environment variables or properties that can be set. These descriptions must
-also include details of any configuration language used.
 
 =head1 DEPENDENCIES
 
-A list of all of the other modules that this module relies upon, including any
-restrictions on versions, and an indication of whether these required modules
-are part of the standard Perl distribution, part of the module's distribution,
-or must be installed separately.
-
 =head1 INCOMPATIBILITIES
 
-A list of any modules that this module cannot be used in conjunction with.
-This may be due to name conflicts in the interface, or competition for system
-or program resources, or due to internal limitations of Perl (for example, many
-modules that use source code filters are mutually incompatible).
-
 =head1 BUGS AND LIMITATIONS
-
-A list of known problems with the module, together with some indication of
-whether they are likely to be fixed in an upcoming release.
-
-Also, a list of restrictions on the features the module does provide: data types
-that cannot be handled, performance issues and the circumstances in which they
-may arise, practical limitations on the size of data sets, special cases that
-are not (yet) handled, etc.
-
-The initial template usually just has:
 
 There are no known bugs in this module.
 
@@ -139,11 +80,10 @@ Patches are welcome.
 =head1 AUTHOR
 
 Ivan Wills - (ivan.wills@gmail.com)
-<Author name(s)>  (<contact address>)
 
 =head1 LICENSE AND COPYRIGHT
 
-Copyright (c) 2015 Ivan Wills (14 Mullion Close, Hornsby Heights, NSW Australia 2077).
+Copyright (c) 2009-2015 Ivan Wills (14 Mullion Close, Hornsby Heights, NSW Australia 2077).
 All rights reserved.
 
 This module is free software; you can redistribute it and/or modify it under
